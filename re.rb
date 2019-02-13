c53e4940689ee7c29c79e8d36f992c3099fb7e1d
aswin-syras <47082111+aswin-syras@users.noreply.github.com>
2019-02-13 14:57:51 +0530

    Update re.rb

--- re.rb
+++ re.rb
@@ -1,6 +1,6 @@
 1. /([345]\d{2})/.match("123456")
 2. str = hellogirlhelloboy
-   m = /hello/
+   m = /^hello/
     m.match(str)
 3. /hello123/.match("hello123four")
 4./hello/i.match("HellodgoodMorning")
